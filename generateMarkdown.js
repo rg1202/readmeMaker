c9ac7bfc0bc9dbb68dd8733d2b0d326f1b04e54a
Rob Gould <rgould.midwest@gmail.com>
2023-11-21 19:46:36 -0600

    base code

---
+++ generateMarkdown.js
@@ -0,0 +1,11 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+
+// TODO: Create a function to generate markdown for README

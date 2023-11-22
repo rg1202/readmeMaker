7edb83408f54bec990c13da09acda78d8306b5c5
Rob Gould <rgould.midwest@gmail.com>
2023-11-21 19:49:03 -0600

    copied code from UCD directory(where I did the work) to add to challenge repo

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,11 +1,95 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+  } else if (license === 'Apache') {
+    return `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
+  } else if (license === 'GPL') {
+    return `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`;
+  } else if (license === 'BSD') {
+    return `![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)`;
+  } else if (license === 'None') {
+    return ``;
+  }
+}
 // If there is no license, return an empty string
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-
+function renderLicenseLink(license) {
+if (license === 'MIT') {
+  return `[License: MIT](https://opensource.org/licenses/MIT)`;
+} else if (license === 'Apache') {
+  return `[License](https://opensource.org/licenses/Apache-2.0)`;
+}
+else if (license === 'GPL') {
+  return `[License: GPL v3](https://www.gnu.org/licenses/gpl-3.0)`;
+}
+else if (license === 'BSD') {
+  return `[License](https://opensource.org/licenses/BSD-3-Clause)`;
+}
+else if (license === 'None') {
+  return ``;
+}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
+function renderLicenseSection(license) {
+if (license === 'MIT') {
+  return `## License
+  This project is licensed under the terms of the MIT license.`;
+}
+else if (license === 'Apache') {
+  return `## License
+  This project is licensed under the terms of the Apache license.`;
+}
+else if (license === 'GPL') {
+  return `## License
+  This project is licensed under the terms of the GPL license.`;
+}
+else if (license === 'BSD') {
+  return `## License
+  This project is licensed under the terms of the BSD license.`;
+}
+else if (license === 'None') {
+  return ``;
+}
+}
+
+
 
 // TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Screenshots](#screenshots)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [License](#license)
+  * [Questions](#questions)
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Screenshots
+  ${data.screenshots}
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  ${data.tests}
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseLink(data.license)}
+  ## Questions
+  For any questions, please contact me at ${data.email} or visit my GitHub page:
+  [${data.github}] 
+`;
+};
+
+module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,10 +1,77 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require(`./generateMarkdown.js`);
 // TODO: Create an array of questions for user input
+    
+const questions = [ 
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.',
+    },
+    {
+        type: 'input',
+        name: 'screenshots',
+        message: 'Please provide any screenshots of your application.',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please list any contributors.',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide any tests for your application and how to run them.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license.',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username.',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address.',
+    },  
+
+];
 
 // TODO: Create a function to write README file
+function writeToFile(fileName, data = '') {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.error(`This is what's up! ${err}`) : console.log('Success!')
+    );
+}
 
- 
 // TODO: Create a function to initialize app
-
+function init() {
+    inquirer.prompt(questions)
+    .then((data) => {
+        writeToFile('README.md', generateMarkdown(data));
+    });
+}   
 // Function call to initialize app
+init();
